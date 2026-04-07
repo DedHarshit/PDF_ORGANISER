3b51be94777ba9be196b1e42723eb0727b356fb9
Harshit Choudhary <hchoudhary1102@gmail.com>
2026-04-07 17:36:06 +0530

    Updated_Stage_2

--- read.py
+++ read.py
@@ -2,17 +2,18 @@ from pypdf import PdfReader
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+import base64
 
 load_dotenv() 
 # STAGE - 1 EXTRACTION
 
-reader = PdfReader("1773859921123.pdf")
+reader = PdfReader("GENERAL-STUDIES-PAPER-IV-QP-CSM-25-010925.pdf")
 page = reader.pages[0]
 text = page.extract_text()
 
 if text and text.strip():
     print("Text found:\n")
-    print(text)
+    #print(text)
 else:
     print("No text found, extracting images...")
     for i, image_file_object in enumerate(page.images):
@@ -37,7 +38,7 @@ if text and text.strip():
         messages=[
             {
                 "role": "system",
-                "content": "You are a help full assistant whos job is to find the correct sorted path/folder for the pdf based on the given text in format of a\b\c",
+                "content":  "You are a document classification assistant. Your task is to assign a concise folder path for the given document.Rules: - Keep the path short and meaningful (2–3 levels max)- Do NOT include year, dates, paper numbers, or unnecessary details- Focus only on the main category and subcategory - Avoid over-specific classification Return ONLY the folder path. No explanation.",
             },
             {
                 "role": "user",
@@ -51,17 +52,41 @@ if text and text.strip():
 )
     
 else:
-    response = client.chat.completions.create(
-       input=[{
-        "role": "user",
-        "content": [
-            {"type": "input_text", "text": "You are a help full assistant whos job is to find the correct sorted path/folder for the pdf based on the given text in format of a\b\c"},
-            {
-                "type": "input_image",
-                "image_url": "https://api.nga.gov/iiif/a2e6da57-3cd1-4235-b20e-95dcaefed6c8/full/!800,800/0/default.jpg",
-            },
-        ],
-    }],
-)
+    print("Using previously extracted image...")
 
-print(response.choices[0].message.content)
+    image_path = None
+    for file in os.listdir():
+        if file.startswith("out-image-"):
+            image_path = file
+            break
+
+    if not image_path:
+        raise Exception("No extracted image found")
+
+    with open(image_path, "rb") as f:
+        image_base64 = base64.b64encode(f.read()).decode("utf-8")
+
+    response = client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a document classification assistant. Your task is to assign a concise folder path for the given document.Rules: - Keep the path short and meaningful (2–3 levels max)- Do NOT include year, dates, paper numbers, or unnecessary details- Focus only on the main category and subcategory - Avoid over-specific classification Return ONLY the folder path. No explanation.",
+            },
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Classify this PDF and return folder path."},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{image_base64}"
+                        }
+                    }
+                ]
+            }
+        ],
+        temperature=0.2,
+        max_tokens=200
+    )
+print(response.choices[0].message.content.strip())
